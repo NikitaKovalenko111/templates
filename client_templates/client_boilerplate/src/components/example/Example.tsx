a0f24526816145995cfa96aa551c3baf29c946f2
NikitaKovalenko111 <nikitakovalenko7219@gmail.com>
2026-04-21 18:40:49 +0500

    added markup and client templates

---
+++ client_templates/client_boilerplate/src/components/example/Example.tsx
@@ -0,0 +1,14 @@
+import type { JSX } from "react";
+import type React from "react";
+
+type PropsType = {}
+
+const Example: React.FC<PropsType> = (): JSX.Element => {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default Example
---
+++ client_templates/client_boilerplate/src/pages/example/ExamplePage.tsx
@@ -0,0 +1,14 @@
+import type { JSX } from "react";
+import type React from "react";
+
+type PropsType = {}
+
+const ExamplePage: React.FC<PropsType> = (): JSX.Element => {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default ExamplePage
